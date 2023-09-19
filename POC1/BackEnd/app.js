bec9e6b16d1e4965638e3191021b1a684fe25d5d
tom <tom.desalmand@epitech.eu>
2023-09-19 16:26:05 +0200

    [ADD] Added python venv for flask backend

---
+++ POC1/BackEnd/app.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const port = 8080
+  
+const server = http.createServer(function (req, res) {
+    res.end()
+})
+  
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port:' + port);
+    }
+})
